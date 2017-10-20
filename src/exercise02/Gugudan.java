738f09f45af7dff5869c2f98cf80a294e9d6f019
구병국 <@10.128.101.110>
2017-10-20 16:55:51 +0900

    bkuk

--- src/exercise02/Gugudan.java
+++ src/exercise02/Gugudan.java
@@ -3,8 +3,8 @@ package exercise02;
 public class Gugudan {
 
 	public static void main(String[] args) {
-		//2단과 3단 계산하여 출력해보세요.
-		//출력형태
+		//2단과 3단 계산하여 출력해보세요.(반복문 사용하지 마세요.)
+		//출력형태(결과 값과 결과값의 사이는 \t사용)
 		//2단
 		//2*1=2	2*2=4 ....
 		
